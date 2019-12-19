9207ea4962e6512f31a09630b17ff0f8a843d9e3
MrMirror21 <mylittlesentry@gmail.com>
2019-12-20 04:37:55 +0900

    Update common_js.js

--- common_js.js
+++ common_js.js
@@ -1,9 +1,26 @@
 window.onload = function() {
+    var battery = Number(document.getElementsByClassName('#battery-left'));
     $(".extinguisher-icon").mouseenter(function() {
         var battery = Number(document.getElementsByClassName('.battery-left'));
         if(battery > 75){
         $(".extinguisher-icon").addClass(".mouseover");
         }
     })
+    /*$(".extinguisher-icon").mouseenter(function() {
+        
+        
+        if (battery > 75) {
+        this.style.color = "green";
+        }
+    })
+    $(".extinguisher-icon").mouseout(function() {
+        this.style.color = "black";
+    })
+    $("#extinguisherchk-1").onclick(function() {
+        document.getElementsByClassName("extinguisher-icon").style.color='red';
+   })
+   console.log(battery * 2);
+*/
 }
 
+    
